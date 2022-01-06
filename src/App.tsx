d1825dd3a7ee3d2db0f71834bfd642bee69c81a4
Aneesh Bhat <aneeshbhatb@gmail.com>
2022-01-07 00:33:42 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App img-bg">
+      <div className="img-attr">
+        Photo by <a href="https://unsplash.com/@chrispee?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Christoph Peich</a> on <a href="https://unsplash.com/s/photos/retro?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
